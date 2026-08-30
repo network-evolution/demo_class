d5068ba3634086c1345451df38e7f59cb19dc94a
networkevolution <networkevolution.in@gmail.com>
2026-08-30 13:09:22 +0530

    env code

--- 01_env_inventory/01_env_demo.py
+++ 01_env_inventory/01_env_demo.py
@@ -1,5 +1,5 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv, dotenv_values
 
 print("Current working directory:", os.getcwd())
 
@@ -8,4 +8,18 @@ loaded = load_dotenv()
 print(loaded)
 
 device_username = os.getenv("DEVICE_USERNAME")
-print(device_username)
+device_password = os.getenv("DEVICE_PASSWORD")
+device_host = os.getenv("DEVICE_HOST")
+missing_value = os.getenv("UNKNOWN_KEY", "ABCD")
+
+dotenv_path = find_dotenv()
+print(f".ENV Path:  {dotenv_path}")
+
+dict_values = dotenv_values()
+
+print(dir(dict_values))
+print(type(dict_values))
+print(dict_values)
+
+# for key in dict_values:
+#     print(key)
