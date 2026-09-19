ab24951bc28e589f5ace08a0f43717570a7dd9c1
networkevolution <networkevolution.in@gmail.com>
2026-09-19 12:18:24 +0530

    added code

--- 01_env_inventory/04_netmiko_demo.py
+++ 01_env_inventory/04_netmiko_demo.py
@@ -7,7 +7,6 @@ from netmiko import ConnectHandler
 load_dotenv()
 
 
-
 with open("inventory.yaml", "r") as f:
     inventory = yaml.safe_load(f.read())
 
@@ -18,6 +17,9 @@ for key, value in sw01.items():
         
 print(sw01)
 
+# import sys
+# sys.exit()
+
 connection = ConnectHandler(
     device_type=sw01["device_type"],
     host=sw01["host"],
@@ -27,6 +29,20 @@ connection = ConnectHandler(
 )
 
 print(connection.send_command("show version"))
+
+
+# with ConnectHandler(
+#     device_type=sw01["device_type"],
+#     host=sw01["host"],
+#     username=sw01["username"],
+#     password=sw01["password"],
+#     port=sw01["port"],
+# ) as connection:
+#     connection.config_mode()
+#     print(connection.check_config_mode())
+# connection.config_mode()
+# print(connection.check_config_mode())
+
 print(connection.send_command("show clock"))
 
 connection.disconnect()
